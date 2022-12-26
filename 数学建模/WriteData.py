d016f9cc76c88f952c636bfa94bac818a89a5c53
Xiao <2085127827@qq.com>
2022-12-26 17:08:53 +0800

    读取写入

---
+++ 数学建模/WriteData.py
@@ -0,0 +1,13 @@
+'''
+Description: 将结果写入文件中
+Author: Xiao
+Date: 2022-12-26 16:53:27
+LastEditTime: 2022-12-26 16:56:48
+LastEditors: Xiao
+'''
+
+
+def MathorCup2022(id, satisfaction, strength, definition, stability):
+    # 用户id 语音通话整体满意度	网络覆盖与信号强度	语音通话清晰度	语音通话稳定性
+    f = open("路径")
+    
