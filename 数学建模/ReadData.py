b7300e408762611dffcf21d40fd383accd7d156a
Xiao <2085127827@qq.com>
2022-12-26 20:31:23 +0800

    第一章

--- 数学建模/ReadData.py
+++ 数学建模/ReadData.py
@@ -2,11 +2,11 @@
 Description: 读取数据
 Author: Xiao
 Date: 2022-12-26 17:01:28
-LastEditTime: 2022-12-26 17:08:38
+LastEditTime: 2022-12-26 17:20:40
 LastEditors: Xiao
 '''
 
 import pandas as pd
 
 def MathorCup2022(path):
-    f = open(path)
+    
